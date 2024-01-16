0ec6bc6a928a28a6549111536651a9658795e9d6
Ulrich Mabou <ulrichmabou@gmail.com>
2024-01-16 11:30:08 +0200

    Add product section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
-    import Hero from './../components/Hero.svelte';
+    import Product from '../components/Product.svelte';
+    import Hero from '../components/Hero.svelte';
 </script>
 
 <main class="flex flex-col">
     <Hero />
+    <Product />
 </main>
