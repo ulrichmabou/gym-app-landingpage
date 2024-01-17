415fc147673eaa0f7dfc7471fbca79cc2f456132
Ulrich Mabou <ulrichmabou@gmail.com>
2024-01-17 12:28:42 +0200

    Add Reviews section

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,6 +2,7 @@
   import CtAs from "./CTAs.svelte";
   import Header from "./Header.svelte";
   import SectionWrapper from "./SectionWrapper.svelte";
+  import Stars from "./Stars.svelte";
 </script>
 
 <SectionWrapper>
@@ -26,20 +27,6 @@
       and much much more.
     </p>
     <CtAs></CtAs>
-    <div class="flex items-center justify-center gap-2 text-base">
-      <p>4.6</p>
-      {#each [0, 1, 2, 3, 4] as index}
-        <div class="grid place-items-center relative">
-          <i class="fa-solid fa-star opacity-0"></i>
-          <div
-            class={"absolute top-0 left-0 grid place-items-center " +
-              (index === 4 ? "w-[60%] overflow-hidden" : " ")}
-          >
-            <i class="fa-solid fa-star text-amber-400"></i>
-          </div>
-        </div>
-      {/each}
-      <p>500+</p>
-    </div>
+    <Stars />
   </div>
 </SectionWrapper>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
     import Product from '../components/Product.svelte';
     import Hero from '../components/Hero.svelte';
+  import Reviews from '../components/Reviews.svelte';
 </script>
 
 <main class="flex flex-col">
     <Hero />
     <Product />
+    <Reviews />
 </main>
