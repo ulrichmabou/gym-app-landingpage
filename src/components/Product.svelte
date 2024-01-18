6c95f52ecb7d2826d4947f9f5df558008d71866a
Ulrich Mabou <ulrichmabou@gmail.com>
2024-01-18 11:28:11 +0200

    Complete landing page

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -39,7 +39,7 @@
   ];
 </script>
 
-<SectionWrapper id="project">
+<SectionWrapper id="product">
   <div
     class="flex flex-col gap-10 sm:gap-14 md:gap-24 flex-1 items-center
     justify-center pb-10 md:pb-14"
